c60292eb1f5e9db73b4bc7f9382d282c88965c5e
marcomarchesano <marco1@bell.net>
2021-12-11 16:02:09 -0500

    fixed the get_files.py with the public github links

--- get_files.py
+++ get_files.py
@@ -1,12 +1,8 @@
 """Downloads all of the required datasets to run the program"""
-
-# DO NOT RUN THIS FILE IT WILL OVERWRITE THE CSV FILES WITH NOTHING
-# WE HAVE TO GET THE URL FOR THE CSV FILES
-
 import requests
 
 # Gets transport_data.csv
-csv_url = ''
+csv_url = 'https://raw.githubusercontent.com/jacobdesousa/csc110_project/main/transport_data.csv'
 
 req = requests.get(csv_url)
 url_content = req.content
@@ -16,7 +12,7 @@ csv_file.write(url_content)
 csv_file.close()
 
 # Gets covid_cases.csv
-csv_url = ''
+csv_url = 'https://raw.githubusercontent.com/jacobdesousa/csc110_project/main/covid_cases.csv'
 
 req = requests.get(csv_url)
 url_content = req.content
@@ -26,7 +22,7 @@ csv_file.write(url_content)
 csv_file.close()
 
 # Gets covid_cases_mod.csv
-csv_url = ''
+csv_url = 'https://raw.githubusercontent.com/jacobdesousa/csc110_project/main/covid_cases_mod.csv'
 
 req = requests.get(csv_url)
 url_content = req.content
