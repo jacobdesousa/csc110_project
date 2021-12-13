b63f8f821217576e2181cddd5795291e77b68343
jacobdesousa <jacobd2009@hotmail.com>
2021-12-13 13:23:36 -0500

    Revert "Added *functioning* slider for average factor, and *functioning* checkboxes for selecting metrics."
    This reverts commit 55cb08cb4f2247ce387006942c9dffef1b760675.

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def average_case_data(data: list[project2.CaseData], grouping: int) -> list[proj
     :param grouping: How many entries to average
     :return: List of CaseData averaged
 
-    Preconditions:
-        - 1 < grouping < len(data)
+    Preconditions
+        - 1 < grouping < 100
     """
     averaged_data = []
 
@@ -39,7 +39,7 @@ def average_case_data(data: list[project2.CaseData], grouping: int) -> list[proj
         for i in range(0, min(grouping, len(data))):
             new_cases += grouped[i].new_cases
             cum_cases += grouped[i].cum_cases
-            if i == min(grouping, math.ceil(len(data) / 2)) - 1:
+            if i == math.ceil(min(grouping, len(data))/2):
                 date = grouped[i].date
         averaged_data.append(project2.CaseData(date, round(new_cases / min(grouping, len(grouped))),
                                                round(cum_cases / min(grouping, len(grouped)))))
@@ -47,7 +47,7 @@ def average_case_data(data: list[project2.CaseData], grouping: int) -> list[proj
     return averaged_data
 
 
-def average_transport_data(data: list[project2.TransportationData], grouping: int) \
+def average_transport_data(data: list[project2.TransportationData], grouping: int)\
         -> list[project2.TransportationData]:
     """
     Calculates the average percentage of each individual form of transportation, the data is
@@ -55,7 +55,7 @@ def average_transport_data(data: list[project2.TransportationData], grouping: in
     average use
 
     Preconditions
-        - 1 < grouping < len(data)
+        - 1 < grouping < 100
     """
     averaged_data = []
 
@@ -84,7 +84,7 @@ def average_transport_data(data: list[project2.TransportationData], grouping: in
             lb += grouped[i].london_buses
             nb += grouped[i].national_buses
             cyc += grouped[i].cycling
-            if i == min(grouping, math.ceil(len(grouped) / 2)) - 1:
+            if i == math.ceil(min(grouping, len(grouped)) / 2):
                 date = grouped[i].date
         averaged_data.append(project2.TransportationData(date,
                                                          round(cars / min(grouping, len(grouped))),
@@ -102,131 +102,150 @@ def average_transport_data(data: list[project2.TransportationData], grouping: in
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
+user_input = int(input('Please enter your value: '))
+df = pd.DataFrame({
+    "Dates": [x.date for x in average_case_data(case_data, user_input)],
+    "Cases": [x.new_cases for x in average_case_data(case_data, user_input)],
+    # "All Motor Vehicles": [x.all_motor_vehicles for x in transport_data],
+    "Cars": [x.cars for x in average_transport_data(transport_data, user_input)],
+    # "Tube": [x.london_tube for x in transport_data]
+    "National Rail": [x.national_rail for x in average_transport_data(transport_data, user_input)],
+    "National Buses": [x.national_buses for x in average_transport_data(transport_data, user_input)],
+    "Cycling": [x.cycling for x in average_transport_data(transport_data, user_input)]
+})
 
-def make_df(grouping: int) -> pd.DataFrame:
-    averaged_case_data = average_case_data(case_data, grouping)
-    averaged_transportation_data = average_transport_data(transport_data, grouping)
-    return pd.DataFrame({
-        "Dates": [x.date for x in averaged_case_data],
-        "Cases": [x.new_cases for x in averaged_case_data],
-        "Cars": [x.cars for x in averaged_transportation_data],
-        "Light Commercial Vehicles": [x.light_commercial_vehicles for x in averaged_transportation_data],
-        "Heavy Goods Vehicles": [x.heavy_goods_vehicles for x in averaged_transportation_data],
-        "All Motor Vehicles": [x.all_motor_vehicles for x in averaged_transportation_data],
-        "National Rail": [x.national_rail for x in averaged_transportation_data],
-        "London Tube": [x.london_tube for x in averaged_transportation_data],
-        "London Buses": [x.london_buses for x in averaged_transportation_data],
-        "National Buses": [x.national_buses for x in averaged_transportation_data],
-        "Cycling": [x.cycling for x in averaged_transportation_data]
-    })
+fig = make_subplots(specs=[[{"secondary_y": True}]])
 
+fig.add_trace(
+    go.Line(x=df.get("Dates"), y=df.get("Cases"), name='Cases'),
+    secondary_y=False
+)
 
-app.layout = html.Div([
-    # html.Header('Select the mode of transportations you would like to see on the graph'),
+fig.add_trace(
+    go.Line(x=df.get("Dates"), y=df.get("Cars"), name='Cars'),
+    secondary_y=True
+)
+
+fig.add_trace(
+    go.Line(x=df.get("Dates"), y=df.get("National Rail"), name='National Rail'),
+    secondary_y=True
+)
+fig.add_trace(
+    go.Line(x=df.get("Dates"), y=df.get("National Buses"), name='National Buses'),
+    secondary_y=True
+)
+
+colours = {
+    'background': '#D2F0F3',
+    'text': '#131313'
+    }
+fig.update_layout(
+    plot_bgcolor=colours['background'],
+    font_color=colours['text'],
+
+)
+
+# fig.layout = dict(template='plotly_dark')
+
+# fig.add_trace(
+#    go.Line(x=df.get("Dates"), y=df.get("Cycling"), name='Cycling'),
+#    secondary_y=True
+# )
+#
+# fig.add_trace(
+#     go.Line(x=df.get("Dates"), y=df.get("Cars"), name='Cars'),
+#     secondary_y=True
+# )
+#
+# fig.add_trace(
+#     go.Line(x=df.get("Dates"), y=df.get("Tube"), name='Tube'),
+#     secondary_y=True
+# )
+
+fig.update_layout(
+    title_text='Cases and All Motor Vehicle Traffic in UK'
+)
+
+fig.update_xaxes(title_text='Date')
+fig.update_yaxes(title_text='Cases', secondary_y=False)
+fig.update_yaxes(title_text='Percent of Transportation used', secondary_y=True)
+
+fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+
+app.layout = html.Div(children=[
+    html.Header('Select the mode of transportations you would like to see on the graph'),
     html.Div(children=[
         html.Label('Modes of Transportation'),
         dcc.Checklist(
             id='modes',
             options=[
-                {'label': 'Cars', 'value': 'Cars'},
-                {'label': 'Light Commercial Vehicles', 'value': 'Light Commercial Vehicles'},
-                {'label': 'Heavy Goods Vehicles', 'value': 'Heavy Goods Vehicles'},
-                {'label': 'All Motor Vehicles', 'value': 'All Motor Vehicles'},
-                {'label': 'National Rail', 'value': 'National Rail'},
-                {'label': 'London Tube', 'value': 'London Tube'},
-                {'label': 'London Buses', 'value': 'London Buses'},
-                {'label': 'National Buses', 'value': 'National Buses'},
-                {'label': 'Cycling', 'value': 'Cycling'},
+                {'label': 'Cars', 'value': 'CARS'},
+                {'label': 'Light Commercial Vehicles', 'value': 'LCV'},
+                {'label': 'Heavy Goods Vehicles', 'value': 'HGV'},
+                {'label': 'All Motor Vehicles', 'value': 'AMV'},
+                {'label': 'National Rail', 'value': 'NR'},
+                {'label': 'London Tube', 'value': 'LT'},
+                {'label': 'London Buses', 'value': 'LB'},
+                {'label': 'National Buses', 'value': 'NB'},
+                {'label': 'Cycling', 'value': 'CYC'},
             ],
             value=[]
         )
     ], style={'width': '100%', 'display': 'inline-block'}),
-    dcc.Graph(id='graphic', ),
-    html.Br(),
-    html.Div(children=[
-        html.Label('Average Factor:'),
-        dcc.Slider(
-            id='average_factor',
-            min=1,
-            max=30,
-            marks={i: i if i % 5 == 0 else '' for i in range(1, 31)},
-            value=5
-        )
-    ], style={'width': '35%'})
 
-    # html.H3("Enter any date below to see the data for the given date (please follow a yyyy/mm/dd "
-    #         "format)"),
-    # html.Div([
-    #     "Input Date: ",
-    #     dcc.Input(id='my-input', value='yyyy/mm/dd', type='text')
-    # ]),
-    # html.Br(),
-    # html.Div(id='my-output')
+    html.Div(children=[
+        dcc.Graph(
+            id='case-graph',
+            figure=fig
+        )
+    ]),
+    html.H3("Enter any date below to see the data for the given date (please follow a yyyy/mm/dd "
+            "format)"),
+    html.Div([
+        "Input Date: ",
+        dcc.Input(id='my-input', value='yyyy/mm/dd', type='text')
+    ]),
+    html.Br(),
+    html.Div(id='my-output')
 ])
 
 
 @app.callback(
-    Output('graphic', 'figure'),
-    Input('average_factor', 'value'),
-    Input('modes', 'value')
+    Output(component_id='graphic', component_property='figure'),
+    Input(component_id='scale', component_property='value'),
+    Input('lines', 'value')
 )
-def update_graph(average_factor, modes):
-    df = make_df(average_factor)
-
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
-    fig.add_trace(
-        go.Line(x=df.get("Dates"), y=df.get("Cases"), name='Cases'),
-        secondary_y=False
-    )
-    for x in modes:
-        fig.add_trace(
-            go.Line(x=df.get("Dates"), y=df.get(x), name=x),
-            secondary_y=True
-        )
-    colours = {
-        'background': '#D2F0F3',
-        'text': '#131313'
-    }
-    fig.update_layout(
-        plot_bgcolor=colours['background'],
-        font_color=colours['text'],
-    )
-
-    fig.update_xaxes(title_text='Date')
-    fig.update_yaxes(title_text='Cases', secondary_y=False)
-    fig.update_yaxes(title_text='Percent of Transportation used', secondary_y=True)
-
-    fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
-    fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
-    return fig
+def update_graph(scale):
+    dff = pd.DataFrame({})
 
 
-# @app.callback(
-#     dash.dependencies.Output(component_id='my-output', component_property='children'),
-#     dash.dependencies.Input(component_id='my-input', component_property='value')
-# )
-# def update_output_div(input_value):
-#     date = datetime.date(int(input_value[0:4]), int(input_value[5:7]), int(input_value[8:10]))
-#     sp_cases = ''
-#     sp_cars = ''
-#     sp_national_rail = ''
-#     sp_national_buses = ''
-#     for a1 in case_data:
-#         if a1.date == date:
-#             sp_cases = str(a1.new_cases)
-#
-#     for a1 in transport_data:
-#         if a1.date == date:
-#             sp_cars = str(a1.cars) + '%'
-#             sp_national_rail = str(a1.national_rail) + '%'
-#             sp_national_buses = str(a1.national_buses) + '%'
-#
-#     return ('|| New Cases: ' + sp_cases +
-#             ', || Cars: ' + sp_cars +
-#             ', || National Rail: ' + sp_national_rail +
-#             ', || National Buses: ' + sp_national_buses +
-#             ' || '
-#             )
+@app.callback(
+    dash.dependencies.Output(component_id='my-output', component_property='children'),
+    dash.dependencies.Input(component_id='my-input', component_property='value')
+)
+def update_output_div(input_value):
+    date = datetime.date(int(input_value[0:4]), int(input_value[5:7]), int(input_value[8:10]))
+    sp_cases = ''
+    sp_cars = ''
+    sp_national_rail = ''
+    sp_national_buses = ''
+    for a1 in case_data:
+        if a1.date == date:
+            sp_cases = str(a1.new_cases)
+
+    for a1 in transport_data:
+        if a1.date == date:
+            sp_cars = str(a1.cars) + '%'
+            sp_national_rail = str(a1.national_rail) + '%'
+            sp_national_buses = str(a1.national_buses) + '%'
+
+    return ('|| New Cases: ' + sp_cases +
+            ', || Cars: ' + sp_cars +
+            ', || National Rail: ' + sp_national_rail +
+            ', || National Buses: ' + sp_national_buses +
+            ' || '
+            )
 
 
 if __name__ == '__main__':
